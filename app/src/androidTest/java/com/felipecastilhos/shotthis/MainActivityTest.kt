9cad43b88372e45b3846be3a7380fb6ecebbefe4
Felipe Castilhos <felipecastilhos@gmail.com>
2020-08-25 16:15:04 -0300

    wip: trying to test with karumi/shot plugin

---
+++ app/src/androidTest/java/com/felipecastilhos/shotthis/MainActivityTest.kt
@@ -0,0 +1,21 @@
+package com.felipecastilhos.shotthis
+
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import com.karumi.shot.ScreenshotTest
+import org.junit.Rule
+import org.junit.Test
+
+class MainActivityTest: ScreenshotTest {
+    @Rule @JvmField
+    var activityRule: IntentsTestRule<MainActivity> =
+        IntentsTestRule(MainActivity::class.java, true, false)
+
+    @Test
+    fun theActivityIsShownProperly() {
+        val firstFragment = startActivity()
+    }
+
+    private fun startActivity(): MainActivity? {
+        return activityRule.launchActivity(null)
+    }
+}
